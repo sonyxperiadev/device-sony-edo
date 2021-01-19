9b357f6bff29eb3194746dde82ccdd1e74d6eed5
Konrad Dybcio <konrad.dybcio@somainline.org>
2021-01-19 17:08:13 +0100

    PlatformConfig.mk: highlight that a75 isn't the optimal target
    Signed-off-by: Konrad Dybcio <konrad.dybcio@somainline.org>

--- PlatformConfig.mk
+++ PlatformConfig.mk
@@ -16,12 +16,14 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
+# TODO: change to A77+A55 when available
 TARGET_CPU_VARIANT := cortex-a75
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
+# TODO: change to A77+A55 when available
 TARGET_2ND_CPU_VARIANT := cortex-a75
 
 BOARD_KERNEL_BASE        := 0x00000000
